1d97505b1c68087947604dadf372c78fda58e704
GabrielMunguia <juayua2011@gmail.com>
2022-07-07 10:35:55 -0600

    se agrego validaciones adicionales para el metodo getPriceUSD

--- helpers/getPriceUSD.js
+++ helpers/getPriceUSD.js
@@ -1,45 +1,68 @@
-const { Convert } = require("easy-currencies");
-const getPriceUSD= async(precio)=>{
-console.log('este es el precio que llega :',precio);
+const getPriceUSD = async (precio) => {
+  console.log("este es el precio que llega :", precio);
   try {
-    const lstMonedas= ["USD","US","EUR","GBP","JPY","CAD","AUD","NZD","CHF","HKD","SGD","SEK","DKK","NOK","KRW","TRY","RUB","INR","BRL","CNY","MYR","IDR","PHP","TWD","THB","VND","ZAR"];
+    const lstMonedas = [
+      "USD",
+      "US",
+      "EUR",
+      "GBP",
+      "JPY",
+      "CAD",
+      "AUD",
+      "NZD",
+      "CHF",
+      "HKD",
+      "SGD",
+      "SEK",
+      "DKK",
+      "NOK",
+      "KRW",
+      "TRY",
+      "RUB",
+      "INR",
+      "BRL",
+      "CNY",
+      "MYR",
+      "IDR",
+      "PHP",
+      "TWD",
+      "THB",
+      "VND",
+      "ZAR",
+    ];
     let monedaProducto = "USD";
     let valor = 0;
-    if(precio.includes("$")){
-      
-        precio =  precio.replace("$","");
+    if (precio.includes("$")) {
+      precio = precio.replace("$", "");
     }
 
-   
-    lstMonedas.forEach(moneda=>{
-        if(precio.includes(moneda)){
-           if(moneda==="US"){
-            monedaProducto="USD";
-            valor=precio.replace("US","");
-           
-            
-           }else{
-            monedaProducto=moneda;
-            valor=precio.replace(monedaProducto,"");
-           }
-           for(let i=0;i<valor.length;i++){
-            if(valor[i]!=="."&& isNaN(valor[i])){
-              valor=valor.replace(".","");
-            }
+    lstMonedas.forEach((moneda) => {
+      if (precio.includes(moneda)) {
+        if (moneda === "US") {
+          monedaProducto = "USD";
+          valor = precio.replace("US", "");
+        } else {
+          monedaProducto = moneda;
+          valor = precio.replace(monedaProducto, "");
         }
-            
-        }
-      })
+        valor = valor.trim();
 
-      console.log('este es el valor  :',valor);
+        for (let i = 0; i < 8; i++) {
+          if ((valor[i] !== "." && isNaN(valor[i])) || valor[i] == "e") {
+            valor = valor.replace(valor[i], "");
+          }
+        }
+      }
+    });
+    valor = valor.replace("e", "");
 
     const precioEnDolares = await Convert(valor).from(monedaProducto).to("USD");
     return precioEnDolares.toFixed(2);
   } catch (error) {
-    0
+    0;
   }
-}
+};
 
-module.exports={
-    getPriceUSD
-}
+module.exports = {
+  getPriceUSD,
+};
